0ffa2356270705173f7af8aa1f118be97b2d6a54
matias <sanchezmati48@gmail.com>
2023-12-19 14:39:32 -0300

    nuevos dialog mas los factory de los mismo me base en los que hay en el figma y los nuevos para el mvp

---
+++ escuelas_flutter/lib/widgets/escuelas_dialog.dart
@@ -0,0 +1,334 @@
+import 'package:escuelas_flutter/extensiones/extensiones.dart';
+import 'package:escuelas_flutter/theming/base.dart';
+import 'package:flutter/material.dart';
+import 'package:full_responsive/full_responsive.dart';
+
+/// {@template EscuelasDialog}
+/// Componente de dialogo general para la app.
+///
+/// Dialog con tres factory:
+/// - exitoso:
+/// Se usa para el procedimiento de una operación y indicarle al usuario que
+/// t0do salio correctamente.
+///
+/// - Fallido:
+/// Se usa para el procedimiento de una operación y indicarle al usuario que
+/// algo fallo.
+///
+/// - Confirmar:
+/// Se usa para el procedimiento de una operación y indicarle al usuario que
+/// requiera el permiso del usuario para ejecutar una función/procedimiento.
+///
+/// El Dialog general puede configurarse y no requiere ningún parámetro
+/// obligatorio, lo que permite pasar varias propiedades para adaptarlo a sus
+/// necesidades específicas.
+/// {@endtemplate}
+class EscuelasDialog extends StatelessWidget {
+  /// {@macro EscuelasDialog}
+  const EscuelasDialog({
+    this.altura = 300,
+    this.conBotonCancelar = false,
+    this.conBotonOutline = false,
+    this.conIconoCerrar = true,
+    this.ancho,
+    this.titulo,
+    this.content,
+    this.onTapConfirmar,
+    this.iconoAlLadoDelTitulo,
+    this.tituloBotonPrincipal,
+    this.tituloDelBotonSecundario,
+    this.colorDeFondoDelBotonSecundario,
+    super.key,
+  });
+
+  /// Altura del dialogo por defecto en `300` (sin .ph) de [EscuelasDialog].
+  final double altura;
+
+  /// Indica si quiere mostrar el Icono de cerrar en el dialogo
+  /// `(por defecto esta el .pop)`de [EscuelasDialog].
+  final bool conIconoCerrar;
+
+  /// Indica si quiere mostrar con botón outline en el dialogo
+  /// `(por defecto esta el .pop)` de [EscuelasDialog].
+  final bool conBotonOutline;
+
+  /// Indica si quiere mostrar con botón de cancelar en el Dialog
+  /// `(por defecto esta el .pop)`de [EscuelasDialog].
+  final bool conBotonCancelar;
+
+  /// Callback para el botón de confirmar,este se ejecuta al presionar
+  /// el botón de [EscuelasDialog].
+  final VoidCallback? onTapConfirmar;
+
+  /// Ancho del dialogo (sin .pw) de [EscuelasDialog].
+  final double? ancho;
+
+  /// Titulo del dialogo de [EscuelasDialog].
+  final String? titulo;
+
+  /// Cuerpo/contenido del dialogo de [EscuelasDialog].
+  final Widget? content;
+
+  /// Titulo del botón de `Confirmar` de [EscuelasDialog].
+  final String? tituloBotonPrincipal;
+
+  /// Icono al lado del titulo de [EscuelasDialog].
+  final IconData? iconoAlLadoDelTitulo;
+
+  /// Titulo del botón de `Cancelar` o `Rechazar` de [EscuelasDialog].
+  final String? tituloDelBotonSecundario;
+
+  /// Color de fondo del botón de `Cancelar` o `Rechazar` de [EscuelasDialog].
+  final Color? colorDeFondoDelBotonSecundario;
+
+  /// `Dialog de éxito`:
+  ///
+  /// Se usa para el procedimiento de una operación y indicarle al usuario que
+  /// t0do salio correctamente.
+  ///
+  /// El Dialog requiere obligatoriamente la propiedad `[onTap]`,`[titulo]` y
+  /// `[context]`.
+  factory EscuelasDialog.exitoso({
+    required BuildContext context,
+    required VoidCallback onTap,
+    required String titulo,
+    double altura = 100,
+    double? ancho,
+  }) {
+    final colores = context.colores;
+
+    return EscuelasDialog(
+      ancho: ancho,
+      altura: altura,
+      onTapConfirmar: onTap,
+      tituloBotonPrincipal: 'aceptar', //TODO(anyone) ver si se hace l10n
+      content: Text(
+        titulo,
+        style: TextStyle(
+          fontSize: 15.pf,
+          fontWeight: FontWeight.w600,
+          color: colores.grisSC,
+        ),
+      ),
+    );
+  }
+
+  /// `Dialog fallido`:
+  ///
+  /// Se usa para el procedimiento de una operación y indicarle al usuario que
+  /// algo fallo.
+  ///
+  /// El Dialog requiere obligatoriamente la propiedad `[onTap]`,`[titulo]` y
+  /// `[context]`.
+  factory EscuelasDialog.fallido({
+    required BuildContext context,
+    required VoidCallback onTap,
+    required String titulo,
+    double altura = 100,
+    double? ancho,
+    String? tituloDelBoton,
+  }) {
+    final colores = context.colores;
+
+    return EscuelasDialog(
+      ancho: ancho,
+      altura: altura,
+      tituloBotonPrincipal: tituloDelBoton,
+      conBotonOutline: true,
+      onTapConfirmar: onTap,
+      content: Text(
+        titulo,
+        style: TextStyle(
+          fontSize: 15.pf,
+          fontWeight: FontWeight.w600,
+          color: colores.grisSC,
+        ),
+      ),
+    );
+  }
+
+  /// `Dialog fallido`:
+  ///
+  /// Se usa para el procedimiento de una operación y indicarle al usuario que
+  /// algo fallo.
+  ///
+  /// El Dialog requiere obligatoriamente la propiedad `[onTapConfirmar]`,
+  /// `[titulo]` y `[context]`.
+  factory EscuelasDialog.confirmar({
+    required BuildContext context,
+    required VoidCallback onTapConfirmar,
+    required String titulo,
+    double altura = 100,
+    double? ancho,
+  }) {
+    final colores = context.colores;
+
+    return EscuelasDialog(
+      ancho: ancho,
+      altura: altura,
+      conBotonCancelar: true,
+      onTapConfirmar: onTapConfirmar,
+      tituloDelBotonSecundario: 'Rechazar', //TODO(anyone) ver si se hace l10n
+      colorDeFondoDelBotonSecundario: colores.error,
+      content: Text(
+        titulo,
+        style: TextStyle(
+          fontSize: 15.pf,
+          fontWeight: FontWeight.w600,
+          color: colores.grisSC,
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final colores = context.colores;
+
+    final titulo = this.titulo;
+
+    return AlertDialog(
+      content: SizedBox(
+        height: altura,
+        width: ancho,
+        child: Column(
+          children: [
+            Row(
+              children: [
+                if (iconoAlLadoDelTitulo != null)
+                  Icon(
+                    iconoAlLadoDelTitulo,
+                    size: 20.pw,
+                    color: colores.onBackground,
+                  ),
+                if (iconoAlLadoDelTitulo != null) SizedBox(width: 5.pw),
+                if (titulo != null)
+                  SizedBox(
+                    width: 150.pw,
+                    child: Text(
+                      titulo,
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                      style: TextStyle(
+                        fontSize: 15.pf,
+                        fontWeight: FontWeight.w600,
+                        color: colores.onBackground,
+                      ),
+                    ),
+                  ),
+                if (conIconoCerrar) const Spacer(),
+                if (conIconoCerrar)
+                  IconButton(
+                    onPressed: () => Navigator.of(context).pop(),
+                    icon: Icon(
+                      Icons.close,
+                      size: 18.pw,
+                      color: colores.onBackground,
+                    ),
+                  ),
+              ],
+            ),
+            content ?? const SizedBox.shrink(),
+          ],
+        ),
+      ),
+      actionsAlignment: MainAxisAlignment.spaceEvenly,
+      actions: [
+        if (conBotonCancelar)
+          EscuelasBoton(
+            onTap: () => Navigator.of(context).pop(),
+            titulo: tituloDelBotonSecundario ??
+                'cancelar', //TODO(anyone) ver si se hace l10n
+            colorDeFondo: colorDeFondoDelBotonSecundario ?? colores.onSecondary,
+          ),
+        if (conBotonOutline)
+          EscuelasBoton.outline(
+            context: context,
+            onTap: () => onTapConfirmar,
+            colorTitulo: colores.grisSC,
+            titulo: tituloBotonPrincipal ??
+                'Volver', //TODO(anyone) ver si se hace l10n
+          )
+        else
+          EscuelasBoton(
+            onTap: () => onTapConfirmar,
+            colorDeFondo: colores.verdeConfirmar,
+            titulo: tituloBotonPrincipal ??
+                'confirmar', //TODO(anyone) ver si se hace l10n
+          ),
+      ],
+    );
+  }
+}
+
+// TODO(anyone) es un boton hasta que este el boton de manu
+class EscuelasBoton extends StatelessWidget {
+  const EscuelasBoton({
+    required this.onTap,
+    required this.titulo,
+    this.radius = 20,
+    this.colorTitulo,
+    this.colorDeFondo,
+    this.estiloDelBoton,
+    super.key,
+  });
+
+  final VoidCallback onTap;
+  final String titulo;
+  final double radius;
+
+  final Color? colorDeFondo;
+  final ButtonStyle? estiloDelBoton;
+  final Color? colorTitulo;
+
+  factory EscuelasBoton.outline({
+    required BuildContext context,
+    required VoidCallback onTap,
+    required String titulo,
+    Color? colorTitulo,
+  }) {
+    final colores = context.colores;
+
+    return EscuelasBoton(
+      onTap: onTap,
+      titulo: titulo,
+      colorTitulo: colorTitulo,
+      estiloDelBoton: ButtonStyle(
+        side: MaterialStateProperty.all(
+          BorderSide(color: colores.onSecondary, width: 1.5.pw),
+        ),
+        shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+          RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20),
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final colores = context.colores;
+
+    return ElevatedButton(
+      onPressed: onTap,
+      style: estiloDelBoton ??
+          ButtonStyle(
+            backgroundColor: MaterialStateProperty.all(colorDeFondo),
+            shape: MaterialStatePropertyAll<RoundedRectangleBorder>(
+              RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(radius),
+              ),
+            ),
+          ),
+      child: Text(
+        titulo.toUpperCase(),
+        style: TextStyle(
+          fontSize: 12.pf,
+          fontWeight: FontWeight.w700,
+          color: colorTitulo ?? colores.onPrimary,
+        ),
+      ),
+    );
+  }
+}
