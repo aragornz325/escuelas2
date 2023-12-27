01d8fc9531442c09e97603b8dee1eea7b9a9ca11
manu <manunavarro95@hotmail.com>
2023-12-27 19:45:15 -0300

    autenticacion terminada, falta login con google, contraseña google creada, mailer de validacion,

--- escuelas_server/lib/server.dart
+++ escuelas_server/lib/server.dart
@@ -1,5 +1,7 @@
+import 'package:mailer/mailer.dart';
+import 'package:mailer/smtp_server.dart';
 import 'package:serverpod/serverpod.dart';
-
+import 'package:serverpod_auth_server/module.dart' as auth;
 import 'package:escuelas_server/src/web/routes/root.dart';
 
 import 'src/generated/protocol.dart';
@@ -27,6 +29,58 @@ void run(List<String> args) async {
   pod.webServer.addRoute(
     RouteStaticDirectory(serverDirectory: 'static', basePath: '/'),
     '/*',
+  );
+
+  auth.AuthConfig.set(
+    auth.AuthConfig(
+      sendValidationEmail: (session, email, validationCode) async {
+        final gmailEmail = session.serverpod.getPassword('gmailEmail')!;
+        final gmailPassword = session.serverpod.getPassword('gmailPassword')!;
+
+        // Crea un cliente SMTP para Gmail.
+        final smtpServer = gmail(gmailEmail, gmailPassword);
+
+        // Crea un mensaje de correo electrónico con el código de validación.
+        final message = Message()
+          ..from = Address(gmailEmail)
+          ..recipients.add(email)
+          ..subject = 'Codigo de validación'
+          ..html = 'Tu codigo de validación es: $validationCode';
+        //Envía el mensaje de correo electrónico.
+        try {
+          await send(message, smtpServer);
+        } catch (_) {
+          // Devuelve falso si no se pudo enviar el correo electrónico.
+          return false;
+        }
+        print('Validation code: $validationCode');
+        return true;
+      },
+      sendPasswordResetEmail: (session, userInfo, validationCode) async {
+        final gmailEmail = session.serverpod.getPassword('gmailEmail')!;
+        final gmailPassword = session.serverpod.getPassword('gmailPassword')!;
+
+        // Crea un cliente SMTP para Gmail.
+        final smtpServer = gmail(gmailEmail, gmailPassword);
+
+        // Crea un mensaje de correo electrónico con el código de validación.
+        final message = Message()
+          ..from = Address(gmailEmail)
+          ..recipients.add(userInfo.email!)
+          ..subject = 'Codigo de validación'
+          ..html = 'Tu codigo de validación es: $validationCode';
+        //Envía el mensaje de correo electrónico.
+        try {
+          await send(message, smtpServer);
+        } catch (_) {
+          // Devuelve falso si no se pudo enviar el correo electrónico.
+          return false;
+        }
+
+        print('Validation code: $validationCode');
+        return true;
+      },
+    ),
   );
 
   // Start the server.
