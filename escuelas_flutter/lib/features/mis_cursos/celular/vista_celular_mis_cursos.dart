6582a21d5ddcf7271680133615ce5266dbc5e26b
Gonzalo Rigoni <gonzalomiguelrigoni@gmail.com>
2024-01-03 09:59:19 -0300

    fixes

--- escuelas_flutter/lib/features/mis_cursos/celular/vista_celular_mis_cursos.dart
+++ escuelas_flutter/lib/features/mis_cursos/celular/vista_celular_mis_cursos.dart
@@ -1,8 +1,6 @@
 import 'package:escuelas_flutter/extensiones/extensiones.dart';
 import 'package:escuelas_flutter/features/mis_cursos/bloc/bloc_mis_cursos.dart';
 import 'package:escuelas_flutter/features/mis_cursos/widgets/item_materia.dart';
-import 'package:escuelas_flutter/widgets/selector_de_fecha/bloc/bloc_selector_de_fecha.dart';
-import 'package:escuelas_flutter/widgets/selector_de_fecha/selector_de_fecha.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:full_responsive/full_responsive.dart';
@@ -19,69 +17,60 @@ class VistaCelularMisCursos extends StatelessWidget {
   Widget build(BuildContext context) {
     final colores = context.colores;
 
-    return BlocBuilder<BlocSelectorDeFecha, BlocSelectorDeFechaEstado>(
-      builder: (context, stateFecha) {
-        return Column(
-          children: [
-            const SelectorDeFecha(),
-            Expanded(
-              child: SingleChildScrollView(
-                child: BlocBuilder<BlocMisCursos, BlocMisCursosEstado>(
-                  builder: (context, state) {
-                    return Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: state.listaCursosConMaterias.keys
-                              .map(
-                                (cursoId) => Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Padding(
-                                      padding: const EdgeInsets.all(8),
-                                      child: Text(
-                                        cursoId.toString(),
-                                        style: TextStyle(
-                                          color: colores.onSecondary,
-                                          fontSize: 13.pf,
-                                          fontWeight: FontWeight.w800,
-                                        ),
-                                      ),
+    return SingleChildScrollView(
+      child: BlocBuilder<BlocMisCursos, BlocMisCursosEstado>(
+        builder: (context, state) {
+          return Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: state.listaCursosConMaterias.keys
+                    .map(
+                      (cursoId) => Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(8),
+                            child: Text(
+                              cursoId.toString(),
+                              style: TextStyle(
+                                color: colores.onSecondary,
+                                fontSize: 13.pf,
+                                fontWeight: FontWeight.w800,
+                              ),
+                            ),
+                          ),
+                          Column(
+                            children: state.listaCursosConMaterias[cursoId]!
+                                .map(
+                                  (materia) => Padding(
+                                    padding: EdgeInsets.only(
+                                      bottom: 10.ph,
                                     ),
-                                    Column(
-                                      children: state
-                                          .listaCursosConMaterias[cursoId]!
-                                          .map(
-                                            (materia) => Padding(
-                                              padding: EdgeInsets.only(
-                                                bottom: 10.ph,
-                                              ),
-                                              child: ItemMateria(
-                                                estaCargada: false,
-                                                estaHabilitado: stateFecha.fecha
-                                                    .isBefore(DateTime.now()),
-                                                materia: materia,
-                                                onTap: () {},
-                                              ),
-                                            ),
-                                          )
-                                          .toList(),
+                                    child: ItemMateria(
+                                      estaCargada: false,
+                                      // TODO(Gon): Usar el valor del bloc de fecha
+                                      estaHabilitado: true
+                                      // stateFecha.fecha
+                                      //     .isBefore(DateTime.now())
+                                      ,
+                                      materia: materia,
+                                      onTap: () {},
                                     ),
-                                  ],
-                                ),
-                              )
-                              .toList(),
-                        ),
-                      ],
-                    );
-                  },
-                ),
+                                  ),
+                                )
+                                .toList(),
+                          ),
+                        ],
+                      ),
+                    )
+                    .toList(),
               ),
-            ),
-          ],
-        );
-      },
+            ],
+          );
+        },
+      ),
     );
   }
 }
