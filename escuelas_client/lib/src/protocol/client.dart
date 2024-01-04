3c03d63134f541916d16c11e8b11fd12971770d9
Gonzalo Rigoni <gonzalomiguelrigoni@gmail.com>
2024-01-03 21:05:43 -0300

    servergen

--- escuelas_client/lib/src/protocol/client.dart
+++ escuelas_client/lib/src/protocol/client.dart
@@ -4,6 +4,8 @@
 // ignore_for_file: library_private_types_in_public_api
 // ignore_for_file: public_member_api_docs
 // ignore_for_file: implementation_imports
+// ignore_for_file: use_super_parameters
+// ignore_for_file: type_literal_in_constant_pattern
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
 import 'package:serverpod_client/serverpod_client.dart' as _i1;
@@ -11,8 +13,9 @@ import 'dart:async' as _i2;
 import 'dart:io' as _i3;
 import 'protocol.dart' as _i4;
 
-class _EndpointExample extends _i1.EndpointRef {
-  _EndpointExample(_i1.EndpointCaller caller) : super(caller);
+/// {@category Endpoint}
+class EndpointExample extends _i1.EndpointRef {
+  EndpointExample(_i1.EndpointCaller caller) : super(caller);
 
   @override
   String get name => 'example';
@@ -35,10 +38,10 @@ class Client extends _i1.ServerpodClient {
           context: context,
           authenticationKeyManager: authenticationKeyManager,
         ) {
-    example = _EndpointExample(this);
+    example = EndpointExample(this);
   }
 
-  late final _EndpointExample example;
+  late final EndpointExample example;
 
   @override
   Map<String, _i1.EndpointRef> get endpointRefLookup => {'example': example};
