d8943b92f4e513a459c160fe219f2631c82cba07
Gonzalo Rigoni <gonzalomiguelrigoni@gmail.com>
2024-01-06 22:47:25 -0300

    a

--- escuelas_flutter/lib/features/perfil_usuario/bloc/bloc_perfil_usuario.dart
+++ escuelas_flutter/lib/features/perfil_usuario/bloc/bloc_perfil_usuario.dart
@@ -27,20 +27,21 @@ class BlocPerfilUsuario
         // TODO(Gon): Eliminar hardcodeo y usar endpoint
         // final usuario = await client.usuario.obtenerDatosDelUsuario();
         emit(
-          BlocPerfilUsuarioEstadoExitoso.desde(state,
-              // TODO(Gon): Usar el usuario del back
-              usuario: usuario
-              usuario: Usuario(
-                idUserInfo: 0,
-                roles: [],
-                dni: '43',
-                ultimaModificacion: DateTime.now(),
-                fechaCreacion: DateTime.now(),
-                nombre: 'gonzalo',
-                apellido: 'higuain',
-                urlFotoDePerfil: 'https://picsum.photos/200/300',
-              ),
-              ),
+          BlocPerfilUsuarioEstadoExitoso.desde(
+            state,
+            // TODO(Gon): Usar el usuario del back
+            // usuario: usuario
+            usuario: Usuario(
+              idUserInfo: 0,
+              roles: [],
+              dni: '43',
+              ultimaModificacion: DateTime.now(),
+              fechaCreacion: DateTime.now(),
+              nombre: 'gonzalo',
+              apellido: 'higuain',
+              urlFotoDePerfil: 'https://picsum.photos/200/300',
+            ),
+          ),
         );
       },
       onError: (e, st) {
