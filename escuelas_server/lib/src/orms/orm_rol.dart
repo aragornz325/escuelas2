14c17caa772c13ecbf4eebef9033337585c603a0
Andreas Meggetto Dursi <meggetto.andreas@c-ven.com>
2024-01-10 19:22:00 -0300

    obtenerUsuariosPorRolSorteados a servicio usuario

--- escuelas_server/lib/src/orms/orm_rol.dart
+++ escuelas_server/lib/src/orms/orm_rol.dart
@@ -164,25 +164,28 @@ class OrmRol extends ORM {
     required int idUsuario,
     required int idRol,
   }) async {
-    final ahora = DateTime.now().toUtc();
     final usuario = await ejecutarOperacionOrm(
       session,
       (session) => Usuario.db.findById(session, idUsuario),
     );
+
     if (usuario == null) {
       throw Exception(
         'Usuario no encontrado',
       );
     }
+
     final rol = await ejecutarOperacionOrm(
       session,
       (session) => RolDeUsuario.db.findById(session, idRol),
     );
+
     if (rol == null) {
       throw Exception(
         'Rol no encontrado',
       );
     }
+
     await ejecutarOperacionOrm(
       session,
       (session) => RelacionUsuarioRol.db.insert(
