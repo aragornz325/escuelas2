ce7a6447c1ab453485915b290188677f5c5e2280
Juan José García <juanjgar998@gmail.com>
2024-03-23 10:50:08 -0300

    Permisos en script de test.

--- escuelas_server/test/escuelas_server_test.dart
+++ escuelas_server/test/escuelas_server_test.dart
@@ -1,4 +1,6 @@
+import 'package:escuelas_commons/escuelas_commons.dart';
 import 'package:escuelas_server/src/endpoints/calificacion_endpoint.dart';
+import 'package:escuelas_server/src/endpoints/rol_endpoint.dart';
 import 'package:escuelas_server/src/endpoints/solicitud_endpoint.dart';
 import 'package:escuelas_server/src/endpoints/usuario_endpoint.dart';
 import 'package:escuelas_server/src/generated/protocol.dart';
@@ -9,6 +11,7 @@ import 'package:escuelas_server/src/orms/orm_usuario.dart';
 import 'package:escuelas_server/src/servicios/servicio_rol.dart';
 import 'package:escuelas_server/src/servicios/servicio_solicitud_nota_mensual.dart';
 import 'package:escuelas_server/src/servicios/servicio_usuario.dart';
+import 'package:rolemissions/rolemissions.dart';
 import 'package:serverpod_auth_server/module.dart';
 import 'package:test/test.dart';
 
@@ -23,6 +26,69 @@ Future<void> main() async {
 
   final authKey = await session.auth.signInUser(11, 'email');
 
-  await UsuarioEndpoint().responderSolicitudDeRegistro(session,
-      estadoDeSolicitud: EstadoDeSolicitud.aprobado, idUsuarioPendiente: 39);
+  final permisosDirectivo = [
+    PermisoDeAsignatura.values,
+    PermisoDeAsistencia.values,
+    PermisoDeCalificacion.values,
+    PermisoDeComision.values,
+    PermisoDeCurso.values,
+    PermisoDeRol.values,
+    PermisoDeSolicitud.values,
+    PermisoDeUsuario.values,
+  ];
+
+  final permisosDocente = [
+    [
+      PermisoDeAsignatura.verAsignatura,
+    ],
+    [PermisoDeAsistencia.crearAsistencia, PermisoDeAsistencia.verAsistencia],
+    [
+      PermisoDeCalificacion.crearCalificacion,
+      PermisoDeCalificacion.verCalificacion,
+    ],
+    [
+      PermisoDeComision.verComision,
+    ],
+    [
+      PermisoDeCurso.verCurso,
+    ],
+    [
+      PermisoDeRol.verRol,
+    ],
+    [
+      PermisoDeSolicitud.verSolicitud,
+    ],
+    [
+      PermisoDeUsuario.verUsuario,
+    ],
+  ];
+
+  final permisosEstudiante = [
+    [
+      PermisoDeAsignatura.verAsignatura,
+    ],
+    [PermisoDeAsistencia.verAsistencia],
+    [
+      PermisoDeCalificacion.verCalificacion,
+    ],
+    [
+      PermisoDeComision.verComision,
+    ],
+    [
+      PermisoDeCurso.verCurso,
+    ],
+    [
+      PermisoDeRol.verRol,
+    ],
+    [
+      PermisoDeSolicitud.verSolicitud,
+    ],
+    [
+      PermisoDeUsuario.verUsuario,
+    ],
+  ];
+
+  await RolEndpoint().crearRol(session,
+      name: 'directivo',
+      permisos: Permisos.fromEnumList(permisosDirectivo).toSerialization(),);
 }
