6c89512eef6bdd60b39aae4bffd6190f6dc5461b
Juan José García <juanjgar998@gmail.com>
2024-03-29 16:56:21 -0300

    Borrar archivos de configuración.

--- escuelas_server/bin/main.dart
+++ escuelas_server/bin/main.dart
@@ -1,7 +1,15 @@
+import 'dart:async';
+
 import 'package:escuelas_server/server.dart';
+import 'package:logging/logging.dart';
 
 /// This is the starting point for your Serverpod server. Typically, there is
 /// no need to modify this file.
-void main(List<String> args) {
-  run(args);
+Future<void> main(List<String> args) async {
+  if (args.contains('staging')) {
+    args[args.indexOf('staging')] = 'development';
+    Logger('SERVER').info('Modo STAGING no disponible. Arrancando server en modo DEVELOPMENT...');
+  }
+  
+  await run(args);
 }
